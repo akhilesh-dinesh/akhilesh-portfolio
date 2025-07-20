379252763fafec14f18b1516f062702e0c293c2f
akhilesh-dinesh <akhilesh.kpd@gmail.com>
2025-07-20 21:16:09 +0530

    expense-tracker project data update

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
       id: 1,
       title: "Front-End Portfolio",
       description: "A personal portfolio showcasing my skills, projects, and experience.",
-      tags: ["React.js", "JavaScript", "Tailwind CSS", "Framer Motion", "EmailJS"],
+      tags: ["React.js", "JavaScript", "Tailwind CSS", "Framer Motion", "EmailJS", "Vite"],
       image: "portfolio.jpg",
       github: "https://github.com/akhilesh-dinesh/akhilesh-portfolio",
       live: window.location.href
@@ -11,8 +11,8 @@ export const projects = [
     {
       id: 2,
       title: "Expense Tracker App (Work in progress...)",
-      description: "A full-stack personal finance tracker with REST API, user-friendly interface, and PostgreSQL database. Built with React, Node.js, and Prisma.",
-      tags: ["React", "Node.js", "PostgreSQL", "Prisma", "TypeScript", "Express", "Vite"],
+      description: "A responsive, full-stack expense management app built using React.js, Node.js, Express, and Prisma. Features includes REST API's, secure authentication, and a PostgreSQL-backed database.",
+      tags: ["React.js", "Node.js", "PostgreSQL", "TypeScript", "Prisma", "Express", "Vite"],
       image: "expense-tracker.png", // Replace later with a screenshot or preview
     }
   ];
