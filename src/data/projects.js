9e35348b31f101ad3b6e3b32283bf84ff53d0975
akhilesh-dinesh <akhilesh.kpd@gmail.com>
2025-08-30 12:44:19 +0530

    rick-and-morty project added

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,6 +10,15 @@ export const projects = [
     },
     {
       id: 2,
+      title: "Rick and Morty",
+      description: "This React 18 + TypeScript SPA uses the Rick & Morty REST API with TanStack Query, Router, and Table to deliver a paginated character list and detailed views. It features persistent state, refresh support, and strong TypeScript typing.",
+      tags: ["React.js", "TypeScript", "TanStack Query", "TanStack Router", " TanStack Router", "Vite"],
+      image: "rick-and-morty.png", // Replace later with a screenshot or preview
+      live: "https://akhilesh-dinesh-rick-and-morty.vercel.app/",
+      github: "https://github.com/akhilesh-dinesh/rick-and-morty-app",
+    },
+    {
+      id: 3,
       title: "Expense Tracker App (Work in progress...)",
       description: "A responsive, full-stack expense management app built using React.js, Node.js, Express, and Prisma. Features includes REST API's, secure authentication, and a PostgreSQL-backed database.",
       tags: ["React.js", "Node.js", "PostgreSQL", "TypeScript", "Prisma", "Express", "Vite"],
